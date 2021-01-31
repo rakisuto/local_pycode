d05af6f8f170c5a560ac198013943ba65c96f749
rakisuto <tama5.bo6bo6@gmail.com>
2021-01-31 17:41:47 +0900

    03 04 05

---
+++ ex03.py
@@ -0,0 +1,6 @@
+
+height = float(input("身長をm単位で入力して下さい > "))
+weight = float(input("体重をkg単位で入力して下さい > "))
+BMI = weight / height**2
+
+print("SUM: ",BMI)
---
+++ ex04.py
@@ -0,0 +1,6 @@
+speed = int(input("時速(km/h) > "))
+
+meter = speed * 1000
+ans = meter / 3600
+
+print("秒速 = ", ans, "m/s")
