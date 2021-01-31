e6ea9d65ad8a237af2d35c41306db6dbe419abac
rakisuto <tama5.bo6bo6@gmail.com>
2021-01-31 17:29:31 +0900

    02

---
+++ ex02.py
@@ -0,0 +1,12 @@
+
+score_english = int(input("英語の成績を入力してください > "))
+score_math = int(input("数学の成績を入力してください > "))
+score_sum = score_english + score_math
+score_avg = score_sum / 2
+
+print("英語の得点: ",score_english)
+print("数学の得点: ",score_math)
+
+print("合計:", score_sum)
+print("平均:", score_avg)
+
